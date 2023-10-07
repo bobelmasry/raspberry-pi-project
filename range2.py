1c6de40c2afd8c892f3b04b852227ffb59d94954
bobelmasry <103034201+bobelmasry@users.noreply.github.com>
2023-10-07 15:59:41 +0300

    Update range2.py

--- range2.py
+++ range2.py
@@ -1,69 +1,42 @@
 from gpiozero import Motor
-from pynput import keyboard
-from time import sleep
-import threading
 
-# Define motor pins
+# Initialize motors
 top_left = Motor(forward=4, backward=22)
 top_right = Motor(forward=17, backward=23)
 bottom_left = Motor(forward=5, backward=6)
 bottom_right = Motor(forward=26, backward=16)
 
-# Initialize a list to store the keys currently being pressed
-keys_pressed = []
+# Function to stop all motors
+def stop_motors():
+    top_left.stop()
+    top_right.stop()
+    bottom_left.stop()
+    bottom_right.stop()
 
-def on_key_release(key):
-    try:
-        # Remove the released key from the list
-        keys_pressed.remove(key.char)
-    except (ValueError, AttributeError):
-        pass
+while True:
+    # Prompt the user for a command
+    command = input("Enter a command (w/a/d/q to quit): ")
 
-def on_key_press(key):
-    try:
-        # Add the pressed key to the list
-        keys_pressed.append(key.char)
-        process_keys()
-    except AttributeError:
-        pass
-
-def process_keys():
-    # Check the keys being pressed and control motors accordingly
-    if 'w' in keys_pressed:
+    if command == 'w':
         top_left.forward()
         top_right.forward()
         bottom_left.forward()
         bottom_right.forward()
-    elif 'a' in keys_pressed:
+    elif command == 'a':
         top_right.forward()
         bottom_left.forward()
         bottom_right.forward()
-    elif 'd' in keys_pressed:
+    elif command == 'd':
         top_left.forward()
         bottom_left.forward()
         bottom_right.forward()
+    elif command == 'q':
+        # Stop all motors and exit the loop
+        stop_motors()
+        break
     else:
-        # Stop all motors if no keys are pressed
-        top_left.stop()
-        top_right.stop()
-        bottom_left.stop()
-        bottom_right.stop()
+        # Invalid input, stop all motors
+        stop_motors()
 
-# Create separate threads for keyboard input and motor control
-keyboard_thread = threading.Thread(target=listen_keyboard)
-motor_thread = threading.Thread(target=process_keys)
-
-def listen_keyboard():
-    # Listen to keyboard input
-    with keyboard.Listener(
-            on_press=on_key_press,
-            on_release=on_key_release) as listener:
-        listener.join()
-
-# Start both threads
-keyboard_thread.start()
-motor_thread.start()
-
-# Wait for the threads to finish
-keyboard_thread.join()
-motor_thread.join()
+# Stop all motors before exiting
+stop_motors()
