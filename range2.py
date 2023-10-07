13d6d7c75e756a17b85b8700fac6147719465e9b
bobelmasry <103034201+bobelmasry@users.noreply.github.com>
2023-10-07 15:40:57 +0300

    Update range2.py

--- range2.py
+++ range2.py
@@ -1,65 +1,69 @@
-import keyboard
-from gpiozero import Motor, LED
+from gpiozero import Motor
+from pynput import keyboard
 from time import sleep
+import threading
 
-top_left = Motor(forward = 4, backward=22)
-top_right = Motor(forward= 17, backward=23)
-bottom_left = Motor(forward= 5, backward=6)
-bottom_right = Motor(forward= 26, backward=16)
+# Define motor pins
+top_left = Motor(forward=4, backward=22)
+top_right = Motor(forward=17, backward=23)
+bottom_left = Motor(forward=5, backward=6)
+bottom_right = Motor(forward=26, backward=16)
 
+# Initialize a list to store the keys currently being pressed
+keys_pressed = []
 
+def on_key_release(key):
+    try:
+        # Remove the released key from the list
+        keys_pressed.remove(key.char)
+    except (ValueError, AttributeError):
+        pass
 
-while True:
-    # Wait for the next event.
-    event = keyboard.read_event()
-    if event.event_type == keyboard.KEY_DOWN and event.name == 'w':
+def on_key_press(key):
+    try:
+        # Add the pressed key to the list
+        keys_pressed.append(key.char)
+        process_keys()
+    except AttributeError:
+        pass
+
+def process_keys():
+    # Check the keys being pressed and control motors accordingly
+    if 'w' in keys_pressed:
         top_left.forward()
         top_right.forward()
-        bottom_left.on()
-        bottom_right.on()
-        print('forward')
-        sleep(.03)
-        top_left.stop()
-        top_right.stop()
-        bottom_left.off()
-        bottom_right.off()
-
-
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'a':
-        top_right.forward()
-        bottom_left.on()
-        bottom_right.on()
-        print('left')
-        sleep(.03)
-        top_right.stop()
-        bottom_left.off()
-        bottom_right.off()
-
-
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'd':
-        top_left.forward()
-        bottom_left.on()
-        bottom_right.on()
-        print('right')
-        sleep(.03)
-        top_left.stop()
-        bottom_left.off()
-        bottom_right.off()
-    # next 4 are for testing
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'i':
-        top_left.forward()
-        top_left.stop()
-    
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'o':
-        top_right.forward()
-        top_right.stop()
-
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'k':
         bottom_left.forward()
-        bottom_left.stop()
-    
-    elif event.event_type == keyboard.KEY_DOWN and event.name == 'l':
         bottom_right.forward()
+    elif 'a' in keys_pressed:
+        top_right.forward()
+        bottom_left.forward()
+        bottom_right.forward()
+    elif 'd' in keys_pressed:
+        top_left.forward()
+        bottom_left.forward()
+        bottom_right.forward()
+    else:
+        # Stop all motors if no keys are pressed
+        top_left.stop()
+        top_right.stop()
+        bottom_left.stop()
         bottom_right.stop()
 
-    else: pass
+# Create separate threads for keyboard input and motor control
+keyboard_thread = threading.Thread(target=listen_keyboard)
+motor_thread = threading.Thread(target=process_keys)
+
+def listen_keyboard():
+    # Listen to keyboard input
+    with keyboard.Listener(
+            on_press=on_key_press,
+            on_release=on_key_release) as listener:
+        listener.join()
+
+# Start both threads
+keyboard_thread.start()
+motor_thread.start()
+
+# Wait for the threads to finish
+keyboard_thread.join()
+motor_thread.join()
